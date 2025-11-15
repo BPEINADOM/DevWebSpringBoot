6fce1dd68064a49627159f1e29467dd5f5f6ab3c
BPEINADOM <bpeinadom@unicartagena.edu.co>
2025-11-15 15:49:45 -0500

    Creacion del proyecto Java Maven SpringBoot

---
+++ ejerciciotaxi/src/main/java/co/edu/udec/ejerciciotaxi/EjerciciotaxiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udec.ejerciciotaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciotaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciotaxiApplication.class, args);
+	}
+
+}
